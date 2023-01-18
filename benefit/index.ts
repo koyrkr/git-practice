dea9f7f7521124c8edc05eaec15362e588399070
Gina <koyrkr@gmail.com>
2023-01-19 03:36:31 +0900

    Feature/manbogi travis (#2)
    * feat: add manbogi-travis-1
    
    * feat: add manbogi-travis-2
    
    * feat: add manbogi-travis-3

--- benefit/index.ts
+++ benefit/index.ts
@@ -1,5 +1,9 @@
 export const benefit = 1;
 
+console.log("mabogi-travis-1");
+console.log("mabogi-travis-2");
+console.log("mabogi-travis-3");
+
 console.log("manbogi-gina-1");
 console.log("manbogi-gina-2");
 console.log("manbogi-gina-3");
